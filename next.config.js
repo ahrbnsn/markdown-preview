84ebcd0cf4e89b05e8efdc128516930e64a7b6d1
Ashley Robinson <github@rbnsn.xyz>
2021-02-15 12:39:01 -0500

    add basic syntax highlighting and mimic github style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
+  extension: /\.(md|mdx)?$/,
+  options: {
+    remarkPlugins: [
+      [require('remark-prism'), {}],
+      [require('remark-admonitions'), {}],
+    ],
+    rehypePlugins: [],
+  },
+});
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-})
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+});
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/default.css';
+import '../styles/prism-style.css';
+import '../styles/admonitions.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
